d1d030969812cd91d9ffc94377d01713f28790c2
MykolaRudnevjs <rudnevmykola@gmail.com>
2021-03-07 15:37:52 +0100

    lear gatsby queries and added component Pizza List

--- src/pages/pizza.js
+++ src/pages/pizza.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzaPage() {
+export default function PizzaPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hej! I`m the Pizza page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
